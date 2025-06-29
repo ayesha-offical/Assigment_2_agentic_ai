74832a0f5abe53d647e24d7410b6009ee6fb36c7
AYESHA FAISAL <160173599+ayesha-offical@users.noreply.github.com>
2025-06-29 20:36:40 +0500

    Async agent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel 
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
 
 load_dotenv()
 gemeni_api_key = os.getenv('GEMINI_API_KEY')
@@ -11,18 +11,22 @@ provider = AsyncOpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
 
+
 model = OpenAIChatCompletionsModel(
     model="gemini-1.5-flash",
     openai_client=provider,
 )
 
+
 aget = Agent(
-    name = "Code helper",
-    instructions=" You are a helpful coding assistant. Answer the user's questions about programming and provide code examples when necessary.when someone ask about rather than code or programing information so say that you can't give information on that.",
+    name = "Dawai Information Agent",
+    instructions=" You are a helpful assistant that provides information about Dawai, a healthcare platform. Answer questions based on the provided context.",
     model=model,
 )
 
+
 user_question = input("Please enter your question: ")
+
 
 result = Runner.run_sync(aget, user_question)
 print(result.final_output)
